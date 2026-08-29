61ee9c54e09e10aba33ffa96dc0a0136cb116639
hieuduongk22bk <hieu.duongk22bk@hcmut.edu.vn>
2026-08-29 13:59:22 +0700

    Add: Curve from bigger and intersec at the middle bottom of banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import { useLanguage } from '../../context/LanguageContext';
-import './Home.css';
+import { Link } from "react-router-dom";
+import { useLanguage } from "../../context/LanguageContext";
+import "./Home.css";
 
 const Home = () => {
   const { t } = useLanguage();
@@ -11,21 +11,24 @@ const Home = () => {
         <img src="/mevabe2.png" alt="Banner mẹ và bé" className="banner-img" />
         <div className="banner-overlay">
           <Link to="/service" className="cta-button">
-            {t('home.discoverServices')}
+            {t("home.discoverServices")}
           </Link>
         </div>
-        
-        {/* SVG overlay tạo hiệu ứng bo cong ở đáy banner */}
-        <svg 
-          className="banner-curve" 
-          viewBox="0 0 1440 100" 
+
+        {/* SVG overlay: viền be rộng 2 đầu, thu hẹp và giao nhau ở giữa */}
+        <svg
+          className="banner-curve"
+          viewBox="0 0 1440 120"
           preserveAspectRatio="none"
           xmlns="http://www.w3.org/2000/svg"
+          style={{ overflow: "visible" }}
         >
-          <path 
-            d="M0,0 Q720,200 1440,0 L1440,100 L0,100 Z" 
-            fill="currentColor" 
-          />
+          {/* BE - vẽ trước, curve gốc (giữ nguyên hình dạng banner) */}
+          <path d="M0,0 Q720,190 1440,0 L1440,120 L0,120 Z" fill="#F5EFE6" />
+
+          {/* WHITE - vẽ sau, đè lên, lệch xuống 25px ở 2 mép, 
+      thu hẹp dần và trùng khớp be tại điểm giữa (x=720) */}
+          <path d="M0,35 Q720,155 1440,35 L1440,120 L0,120 Z" fill="white" />
         </svg>
       </section>
     </div>
